8977ebe5999711c8e8cdc70eab2316f0da16acb8
Agnes Suzan <75346405+agszn@users.noreply.github.com>
2026-01-07 16:06:12 +0530

    Update urls.py

--- accident/urls.py
+++ accident/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('api/sensor-data/', sensor_data, name='sensor_data'),
+    path('sensor-data/', sensor_data, name='sensor_data'),
     path('sensor_dashboard/', sensor_dashboard, name='sensor_dashboard'),
     path('analytics/', data_analytics, name='data_analytics'),
 ]
+
